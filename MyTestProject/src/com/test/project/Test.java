ab374643b3f87eeda71298a5693ed50dfc04ad94
Asad Mustafa <Asad Mustafa>
2015-01-08 19:55:29 +0500

    second commit

--- MyTestProject/src/com/test/project/Test.java
+++ MyTestProject/src/com/test/project/Test.java
@@ -3,6 +3,7 @@ package com.test.project;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		String str = "Hello World!";
+		System.out.println(str);
 	}
 }
